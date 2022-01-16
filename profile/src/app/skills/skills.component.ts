1a7b34dd69686ad7bee99fd7342974c066b90b7e
Andres Espinosa <andres.espinosa@vivaair.com>
2022-01-15 23:16:07 -0500

    add and design skills component

---
+++ profile/src/app/skills/skills.component.ts
@@ -0,0 +1,30 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-skills',
+  templateUrl: './skills.component.html',
+  styleUrls: ['./skills.component.css']
+})
+export class SkillsComponent implements OnInit {
+  @Input("user") user: any;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  skillCategories(){
+    let categories = this.user.strengths.map( (strength: any) => strength.proficiency);
+    console.log(new Set(categories))
+    return Array.from(new Set(categories));
+  }
+
+  skillsByCategory(category: any) {
+    let skills = this.user.strengths.filter((skill: any) => skill.proficiency == category);
+    console.log(skills)
+    return skills;
+  }
+
+
+}
