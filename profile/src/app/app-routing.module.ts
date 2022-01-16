9800dfa253805753273fa507c1dff4c15580a8a8
Andres Espinosa <andres.espinosa@vivaair.com>
2022-01-15 23:12:32 -0500

    Create user component

--- profile/src/app/app-routing.module.ts
+++ profile/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserComponent } from './user/user.component';
+import { SkillsComponent } from './skills/skills.component';
+import { DataComponent } from './data/data.component';
+import { ExperienceComponent } from './experience/experience.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: UserComponent},
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
