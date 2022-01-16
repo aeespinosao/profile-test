785ca6b80a1b19818a456d9dd8ea5e9124e5c157
Andres Espinosa <andres.espinosa@vivaair.com>
2022-01-15 23:14:19 -0500

    create data component

---
+++ profile/src/app/data/data.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-data',
+  templateUrl: './data.component.html',
+  styleUrls: ['./data.component.css']
+})
+export class DataComponent implements OnInit {
+  @Input("user") user: any;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
